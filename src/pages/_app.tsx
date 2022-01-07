3ea35eebe71fa4d72d5d2669e64450adcb579719
Patrick Obermeier <pato@xxxlgroup.com>
2022-01-07 14:39:55 +0100

    apply custom useCookieBanner & useGoogleTagManager hooks

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,12 @@
 import "styles/globals.css";
 import type { AppProps } from "next/app";
+import useCookieBanner from "hooks/useCookieBanner";
+import useGoogleTagManager from "hooks/useGoogleTagManager";
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
+  useCookieBanner();
+  useGoogleTagManager();
+
   return <Component {...pageProps} />;
 };
 
