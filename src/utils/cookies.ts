2cc98c9e53c5cd51fd6eb776c972ad9b9663d84f
Patrick Obermeier <pato@xxxlgroup.com>
2022-01-07 14:37:38 +0100

    import util-functions to read & write cookies

---
+++ src/utils/cookies.ts
@@ -0,0 +1,22 @@
+export const setCookie = (
+  cookieName: string,
+  cookieValue: string,
+  expirationDays: number
+): void => {
+  const date = new Date();
+
+  date.setTime(date.getTime() + expirationDays * 24 * 60 * 60 * 1000);
+  document.cookie = `${cookieName}=${cookieValue};expires=${date.toUTCString()};path=/`;
+};
+
+export const getCookie = (cookieName: string): string => {
+  const allStoredCookies = document.cookie.split('; ');
+
+  const [foundCookie] = allStoredCookies.filter((cookie) => cookie.split('=').includes(cookieName));
+
+  return foundCookie;
+};
+
+export const checkCookie = (cookieName: string, callback: () => void): void => {
+  !getCookie(cookieName) && setTimeout(callback, 0);
+};
