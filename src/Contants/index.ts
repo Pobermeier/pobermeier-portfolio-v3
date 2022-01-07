27212bb803035503b280f2b9b48c1a298b1fd29c
Patrick Obermeier <pato@xxxlgroup.com>
2022-01-07 14:37:51 +0100

    create constants

---
+++ src/Contants/index.ts
@@ -0,0 +1,551 @@
+export const keys = {
+  "0": {
+    keyCode: 48,
+    keyName: "0",
+    keyNameAsString: "Digit0",
+  },
+  "1": {
+    keyCode: 49,
+    keyName: "1",
+    keyNameAsString: "Digit1",
+  },
+  "2": {
+    keyCode: 50,
+    keyName: "2",
+    keyNameAsString: "Digit2",
+  },
+  "3": {
+    keyCode: 51,
+    keyName: "3",
+    keyNameAsString: "Digit3",
+  },
+  "4": {
+    keyCode: 52,
+    keyName: "4",
+    keyNameAsString: "Digit4",
+  },
+  "5": {
+    keyCode: 53,
+    keyName: "5",
+    keyNameAsString: "Digit5",
+  },
+  "6": {
+    keyCode: 54,
+    keyName: "6",
+    keyNameAsString: "Digit6",
+  },
+  "7": {
+    keyCode: 55,
+    keyName: "7",
+    keyNameAsString: "Digit7",
+  },
+  "8": {
+    keyCode: 56,
+    keyName: "8",
+    keyNameAsString: "Digit8",
+  },
+  "9": {
+    keyCode: 57,
+    keyName: "9",
+    keyNameAsString: "Digit9",
+  },
+  backspace: {
+    keyCode: 8,
+    keyName: "Backspace",
+    keyNameAsString: "Backspace",
+  },
+  tab: {
+    keyCode: 9,
+    keyName: "Tab",
+    keyNameAsString: "Tab",
+  },
+  enter: {
+    keyCode: 13,
+    keyName: "Enter",
+    keyNameAsString: "Enter",
+  },
+  "shift(left)": {
+    keyCode: 16,
+    keyName: "Shift",
+    keyNameAsString: "ShiftLeft",
+  },
+  "shift(right)": {
+    keyCode: 16,
+    keyName: "Shift",
+    keyNameAsString: "ShiftRight",
+  },
+  "ctrl(left)": {
+    keyCode: 17,
+    keyName: "Control",
+    keyNameAsString: "ControlLeft",
+  },
+  "ctrl(right)": {
+    keyCode: 17,
+    keyName: "Control",
+    keyNameAsString: "ControlRight",
+  },
+  "alt(left)": {
+    keyCode: 18,
+    keyName: "Alt",
+    keyNameAsString: "AltLeft",
+  },
+  "alt(right)": {
+    keyCode: 18,
+    keyName: "Alt",
+    keyNameAsString: "AltRight",
+  },
+  "pause/break": {
+    keyCode: 19,
+    keyName: "Pause",
+    keyNameAsString: "Pause",
+  },
+  "caps lock": {
+    keyCode: 20,
+    keyName: "CapsLock",
+    keyNameAsString: "CapsLock",
+  },
+  escape: {
+    keyCode: 27,
+    keyName: "Escape",
+    keyNameAsString: "Escape",
+  },
+  space: {
+    keyCode: 32,
+    keyName: "",
+    keyNameAsString: "Space",
+  },
+  "page up": {
+    keyCode: 33,
+    keyName: "PageUp",
+    keyNameAsString: "PageUp",
+  },
+  "page down": {
+    keyCode: 34,
+    keyName: "PageDown",
+    keyNameAsString: "PageDown",
+  },
+  end: {
+    keyCode: 35,
+    keyName: "End",
+    keyNameAsString: "End",
+  },
+  home: {
+    keyCode: 36,
+    keyName: "Home",
+    keyNameAsString: "Home",
+  },
+  "left arrow": {
+    keyCode: 37,
+    keyName: "ArrowLeft",
+    keyNameAsString: "ArrowLeft",
+  },
+  "up arrow": {
+    keyCode: 38,
+    keyName: "ArrowUp",
+    keyNameAsString: "ArrowUp",
+  },
+  "right arrow": {
+    keyCode: 39,
+    keyName: "ArrowRight",
+    keyNameAsString: "ArrowRight",
+  },
+  "down arrow": {
+    keyCode: 40,
+    keyName: "ArrowDown",
+    keyNameAsString: "ArrowDown",
+  },
+  "print screen": {
+    keyCode: 44,
+    keyName: "PrintScreen",
+    keyNameAsString: "PrintScreen",
+  },
+  insert: {
+    keyCode: 45,
+    keyName: "Insert",
+    keyNameAsString: "Insert",
+  },
+  delete: {
+    keyCode: 46,
+    keyName: "Delete",
+    keyNameAsString: "Delete",
+  },
+  a: {
+    keyCode: 65,
+    keyName: "a",
+    keyNameAsString: "KeyA",
+  },
+  b: {
+    keyCode: 66,
+    keyName: "b",
+    keyNameAsString: "KeyB",
+  },
+  c: {
+    keyCode: 67,
+    keyName: "c",
+    keyNameAsString: "KeyC",
+  },
+  d: {
+    keyCode: 68,
+    keyName: "d",
+    keyNameAsString: "KeyD",
+  },
+  e: {
+    keyCode: 69,
+    keyName: "e",
+    keyNameAsString: "KeyE",
+  },
+  f: {
+    keyCode: 70,
+    keyName: "f",
+    keyNameAsString: "KeyF",
+  },
+  g: {
+    keyCode: 71,
+    keyName: "g",
+    keyNameAsString: "KeyG",
+  },
+  h: {
+    keyCode: 72,
+    keyName: "h",
+    keyNameAsString: "KeyH",
+  },
+  i: {
+    keyCode: 73,
+    keyName: "i",
+    keyNameAsString: "KeyI",
+  },
+  j: {
+    keyCode: 74,
+    keyName: "j",
+    keyNameAsString: "KeyJ",
+  },
+  k: {
+    keyCode: 75,
+    keyName: "k",
+    keyNameAsString: "KeyK",
+  },
+  l: {
+    keyCode: 76,
+    keyName: "l",
+    keyNameAsString: "KeyL",
+  },
+  m: {
+    keyCode: 77,
+    keyName: "m",
+    keyNameAsString: "KeyM",
+  },
+  n: {
+    keyCode: 78,
+    keyName: "n",
+    keyNameAsString: "KeyN",
+  },
+  o: {
+    keyCode: 79,
+    keyName: "o",
+    keyNameAsString: "KeyO",
+  },
+  p: {
+    keyCode: 80,
+    keyName: "p",
+    keyNameAsString: "KeyP",
+    // eslint-disable-next-line max-lines
+  },
+  q: {
+    keyCode: 81,
+    keyName: "q",
+    keyNameAsString: "KeyQ",
+  },
+  r: {
+    keyCode: 82,
+    keyName: "r",
+    keyNameAsString: "KeyR",
+  },
+  s: {
+    keyCode: 83,
+    keyName: "s",
+    keyNameAsString: "KeyS",
+  },
+  t: {
+    keyCode: 84,
+    keyName: "t",
+    keyNameAsString: "KeyT",
+  },
+  u: {
+    keyCode: 85,
+    keyName: "u",
+    keyNameAsString: "KeyU",
+  },
+  v: {
+    keyCode: 86,
+    keyName: "v",
+    keyNameAsString: "KeyV",
+  },
+  w: {
+    keyCode: 87,
+    keyName: "w",
+    keyNameAsString: "KeyW",
+  },
+  x: {
+    keyCode: 88,
+    keyName: "x",
+    keyNameAsString: "KeyX",
+  },
+  y: {
+    keyCode: 89,
+    keyName: "y",
+    keyNameAsString: "KeyY",
+  },
+  z: {
+    keyCode: 90,
+    keyName: "z",
+    keyNameAsString: "KeyZ",
+  },
+  "left window key": {
+    keyCode: 91,
+    keyName: "Meta",
+    keyNameAsString: "MetaLeft",
+  },
+  "right window key": {
+    keyCode: 92,
+    keyName: "Meta",
+    keyNameAsString: "MetaRight",
+  },
+  "select key (Context Menu)": {
+    keyCode: 93,
+    keyName: "ContextMenu",
+    keyNameAsString: "ContextMenu",
+  },
+  "numpad 0": {
+    keyCode: 96,
+    keyName: "0",
+    keyNameAsString: "Numpad0",
+  },
+  "numpad 1": {
+    keyCode: 97,
+    keyName: "1",
+    keyNameAsString: "Numpad1",
+  },
+  "numpad 2": {
+    keyCode: 98,
+    keyName: "2",
+    keyNameAsString: "Numpad2",
+  },
+  "numpad 3": {
+    keyCode: 99,
+    keyName: "3",
+    keyNameAsString: "Numpad3",
+  },
+  "numpad 4": {
+    keyCode: 100,
+    keyName: "4",
+    keyNameAsString: "Numpad4",
+  },
+  "numpad 5": {
+    keyCode: 101,
+    keyName: "5",
+    keyNameAsString: "Numpad5",
+  },
+  "numpad 6": {
+    keyCode: 102,
+    keyName: "6",
+    keyNameAsString: "Numpad6",
+  },
+  "numpad 7": {
+    keyCode: 103,
+    keyName: "7",
+    keyNameAsString: "Numpad7",
+  },
+  "numpad 8": {
+    keyCode: 104,
+    keyName: "8",
+    keyNameAsString: "Numpad8",
+  },
+  "numpad 9": {
+    keyCode: 105,
+    keyName: "9",
+    keyNameAsString: "Numpad9",
+  },
+  multiply: {
+    keyCode: 106,
+    keyName: "*",
+    keyNameAsString: "NumpadMultiply",
+  },
+  add: {
+    keyCode: 107,
+    keyName: "+",
+    keyNameAsString: "NumpadAdd",
+  },
+  subtract: {
+    keyCode: 109,
+    keyName: "-",
+    keyNameAsString: "NumpadSubtract",
+  },
+  "decimal point": {
+    keyCode: 110,
+    keyName: ".",
+    keyNameAsString: "NumpadDecimal",
+  },
+  divide: {
+    keyCode: 111,
+    keyName: "/",
+    keyNameAsString: "NumpadDivide",
+  },
+  f1: {
+    keyCode: 112,
+    keyName: "F1",
+    keyNameAsString: "F1",
+  },
+  f2: {
+    keyCode: 113,
+    keyName: "F2",
+    keyNameAsString: "F2",
+  },
+  f3: {
+    keyCode: 114,
+    keyName: "F3",
+    keyNameAsString: "F3",
+  },
+  f4: {
+    keyCode: 115,
+    keyName: "F4",
+    keyNameAsString: "F4",
+  },
+  f5: {
+    keyCode: 116,
+    keyName: "F5",
+    keyNameAsString: "F5",
+  },
+  f6: {
+    keyCode: 117,
+    keyName: "F6",
+    keyNameAsString: "F6",
+  },
+  f7: {
+    keyCode: 118,
+    keyName: "F7",
+    keyNameAsString: "F7",
+  },
+  f8: {
+    keyCode: 119,
+    keyName: "F8",
+    keyNameAsString: "F8",
+  },
+  f9: {
+    keyCode: 120,
+    keyName: "F9",
+    keyNameAsString: "F9",
+  },
+  f10: {
+    keyCode: 121,
+    keyName: "F10",
+    keyNameAsString: "F10",
+  },
+  f11: {
+    keyCode: 122,
+    keyName: "F11",
+    keyNameAsString: "F11",
+  },
+  f12: {
+    keyCode: 123,
+    keyName: "F12",
+    keyNameAsString: "F12",
+  },
+  "num lock": {
+    keyCode: 144,
+    keyName: "NumLock",
+    keyNameAsString: "NumLock",
+  },
+  "scroll lock": {
+    keyCode: 145,
+    keyName: "ScrollLock",
+    keyNameAsString: "ScrollLock",
+  },
+  "audio volume mute": {
+    keyCode: 173,
+    keyName: "AudioVolumeMute",
+    keyNameAsString: "",
+  },
+  "audio volume down": {
+    keyCode: 174,
+    keyName: "AudioVolumeDown",
+    keyNameAsString: "",
+  },
+  "audio volume up": {
+    keyCode: 175,
+    keyName: "AudioVolumeUp",
+    keyNameAsString: "",
+  },
+  "media player": {
+    keyCode: 181,
+    keyName: "LaunchMediaPlayer",
+    keyNameAsString: "",
+  },
+  "launch application 1": {
+    keyCode: 182,
+    keyName: "LaunchApplication1",
+    keyNameAsString: "",
+  },
+  "launch application 2": {
+    keyCode: 183,
+    keyName: "LaunchApplication2",
+    keyNameAsString: "",
+  },
+  "semi-colon": {
+    keyCode: 186,
+    keyName: ";",
+    keyNameAsString: "Semicolon",
+  },
+  "equal sign": {
+    keyCode: 187,
+    keyName: "=",
+    keyNameAsString: "Equal",
+  },
+  comma: {
+    keyCode: 188,
+    keyName: ",",
+    keyNameAsString: "Comma",
+  },
+  dash: {
+    keyCode: 189,
+    keyName: "-",
+    keyNameAsString: "Minus",
+  },
+  period: {
+    keyCode: 190,
+    keyName: ".",
+    keyNameAsString: "Period",
+  },
+  "forward slash": {
+    keyCode: 191,
+    keyName: "/",
+    keyNameAsString: "Slash",
+  },
+  "Backquote/Grave accent": {
+    keyCode: 192,
+    keyName: "`",
+    keyNameAsString: "Backquote",
+  },
+  "open bracket": {
+    keyCode: 219,
+    keyName: "[",
+    keyNameAsString: "BracketLeft",
+  },
+  "back slash": {
+    keyCode: 220,
+    keyName: "\\",
+    keyNameAsString: "Backslash",
+  },
+  "close bracket": {
+    keyCode: 221,
+    keyName: "]",
+    keyNameAsString: "BracketRight",
+  },
+  "single quote": {
+    keyCode: 222,
+    keyName: "'",
+    keyNameAsString: "Quote",
+  },
+};
+
+export const isProd = process.env.NODE_ENV === "production";
+export const isDev = !isProd;
