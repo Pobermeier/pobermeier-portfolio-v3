01e0b37d0f19b7b80fe9a3e93718378620074195
Patrick Obermeier <pato@xxxlgroup.com>
2022-01-07 14:38:34 +0100

    create custom useGoogleTagManager-hook

---
+++ src/hooks/useGoogleTagManager.ts
@@ -0,0 +1,21 @@
+import { useEffect } from "react";
+import Router from "next/router";
+
+// utils
+import { pushGtmPageViewToDataLayer } from "utils/gtm";
+
+const useGoogleTagManager = (): void => {
+  useEffect(() => {
+    const handleRouteChange = (url: string): void => {
+      pushGtmPageViewToDataLayer(url);
+    };
+
+    Router.events.on("routeChangeComplete", handleRouteChange);
+
+    return () => {
+      Router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, []);
+};
+
+export default useGoogleTagManager;
