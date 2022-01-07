2bbfbcb626e99a261d1c7e4b3f630b7eb6cbf7a9
Patrick Obermeier <pato@xxxlgroup.com>
2022-01-07 15:17:35 +0100

    update cookie-banner with tailwind-styles

--- src/hooks/useCookieBanner.ts
+++ src/hooks/useCookieBanner.ts
@@ -7,19 +7,24 @@ const useCookieBanner = (): void => {
 
   const showCookieAlert = useCallback((): void => {
     const template = `
-      <div id="cookie-info" class="cookie-banner">
-        <div class="cookie-banner__content">
-          <div class="cookie-banner__text">This website uses cookies. If you continue to use the website, we assume your consent.</div>
-          <div class="cookie-banner__buttons">
-            <button id="accept-cookies" class="cookie-banner__accept-button gtm-tracked-link">Continue</button>
-            <button id="open-privacy-notice" class="cookie-banner__info-button gtm-tracked-link">Policy</button>
+      <section class="w-full p-5 lg:px-24 bg-gray-600">
+        <div class="md:flex items-center -mx-3">
+          <div class="md:flex-1 px-3 mb-5 md:mb-0">
+            <p class="text-center md:text-left text-white text-sm leading-tight md:pr-12">This website uses cookies. If you continue to use the website, we assume your consent.</p>
+          </div>
+          <div class="px-3 flex-1 items-center">
+            <button class="py-2 px-8 bg-gray-800 hover:bg-gray-900 text-white rounded font-bold text-xs shadow-xl sm:mr-3 mb-2 sm:mb-0" id="open-privacy-notice">Privacy policy</button>
+            <button class="py-2 px-8 bg-green-400 hover:bg-green-500 text-white rounded font-bold text-xs shadow-xl mb-2 sm:mb-0" id="accept-cookies">Accept cookies</button>
           </div>
         </div>
-      </div>`;
+      </section>
+    `;
+
+    if (document.getElementById("#cookie-info")) return;
 
     const wrapper = document.createElement("div");
+    wrapper.id = "cookie-info";
     wrapper.style.position = "fixed";
-    wrapper.style.padding = "0 1.5rem";
     wrapper.style.bottom = "0";
     wrapper.style.left = "0";
     wrapper.style.width = "100%";
@@ -37,7 +42,7 @@ const useCookieBanner = (): void => {
 
     document.addEventListener("click", (event) => {
       if ((event.target as HTMLButtonElement).id === "open-privacy-notice") {
-        router.push("/privacy");
+        router.push("/privacy", undefined, { shallow: true });
       }
     });
   }, [router]);
