584065ce3f1c8d1a29f22e51ed6acde5563aa15e
Patrick Obermeier <pato@xxxlgroup.com>
2022-01-07 17:03:32 +0100

    fix error in lint-staged config

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,15 +1,18 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const path = require("path");
+
 module.exports = {
   // Type check TypeScript files
   "**/*.(ts|tsx)": () => "yarn ts-validate",
 
   // Lint then format TypeScript and JavaScript files
-  "**/*.(ts|tsx|js|jsx)": (filenames) => [
+  "**/*.(ts|tsx|js)": (filenames) => [
     `yarn lint:fix --file ${filenames
       .map((f) => path.relative(process.cwd(), f))
       .join(" --file ")}`,
-    `yarn prettier --write ${filenames.join(" ")}`,
+    `yarn format ${filenames.join(" ")}`,
   ],
 
   // Format MarkDown and JSON
-  "**/*.(md|json)": (filenames) => `yarn prettier --write ${filenames.join(" ")}`,
+  "**/*.(md|json)": (filenames) => `yarn format ${filenames.join(" ")}`,
 };
