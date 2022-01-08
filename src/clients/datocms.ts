851eab14ee6d1ae3c1e9a12f3b0095b6aeb0b35b
Patrick Obermeier <pato@xxxlgroup.com>
2022-01-08 15:28:18 +0100

    create graphql-client to communicate with DatoCMS

---
+++ src/clients/datocms.ts
@@ -0,0 +1,14 @@
+import { GraphQLClient } from "graphql-request";
+
+export function request(query: string, variables = null, isPreview = false) {
+  const endpoint = isPreview
+    ? `https://graphql.datocms.com/preview`
+    : `https://graphql.datocms.com/`;
+
+  const client = new GraphQLClient(endpoint, {
+    headers: {
+      authorization: `Bearer ${process.env.DATOCMS_API_TOKEN}`,
+    },
+  });
+  return client.request(query, variables);
+}
