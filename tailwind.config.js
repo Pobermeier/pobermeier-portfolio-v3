58ab0807e7825add3c6a4b3e126ec1ff269cbf66
Patrick Obermeier <pato@xxxlgroup.com>
2022-01-09 18:41:38 +0100

    add new font to tailwind-config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,14 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
-  content: [
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Inter var", ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [],
-}
+};
