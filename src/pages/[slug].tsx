8ee7b5b90c294910cac258bc6e22e62b17cd7d42
Patrick Obermeier <pato@xxxlgroup.com>
2022-01-09 22:15:26 +0100

    add logic to render navbar & footer using data from datoCMS

--- src/pages/[slug].tsx
+++ src/pages/[slug].tsx
@@ -1,52 +1,52 @@
 import { GetStaticPropsContext } from "next";
-import { gql } from "graphql-request";
+
+import Navbar, { NavbarProps } from "components/Navigation/Navbar";
+import Footer, { FooterProps } from "components/Navigation/Footer";
 import { request } from "clients/datocms";
+import { CmsData } from "models/datoCMS";
+import { GET_PAGE_DATA_QUERY } from "graphql/queries/getPageData";
+import { GET_ALL_PAGE_SLUGS_QUERY } from "graphql/queries/getAllPageSlugs";
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-const Page = ({ data }: { data: any }) => {
-  return <pre>{JSON.stringify(data)}</pre>;
+interface Props {
+  data: CmsData;
+}
+
+const Page = ({
+  data: {
+    page: { navbar, footer },
+  },
+}: Props) => {
+  const renderNavbar = () => {
+    const { socialMediaIcons, navigationLinks, callToActions } = navbar as NavbarProps;
+
+    return (
+      <Navbar
+        socialMediaIcons={socialMediaIcons}
+        navigationLinks={navigationLinks}
+        callToActions={callToActions}
+      />
+    );
+  };
+
+  const renderFooter = () => {
+    const { copyrightText, navigationLinks, socialMediaIcons } = footer as FooterProps;
+
+    return (
+      <Footer
+        copyrightText={copyrightText}
+        navigationLinks={navigationLinks}
+        socialMediaIcons={socialMediaIcons}
+      />
+    );
+  };
+
+  return (
+    <div className="relative bg-gray-50">
+      {navbar && renderNavbar()}
+      {footer && renderFooter()}
+    </div>
+  );
 };
-
-const GET_ALL_PAGE_SLUGS_QUERY = gql`
-  query GetAllSlugs {
-    allPages {
-      slug
-    }
-  }
-`;
-
-const GET_PAGE_DATA_QUERY = gql`
-  query GetPageData($slug: String!) {
-    page(filter: { slug: { eq: $slug } }) {
-      title
-      createdAt
-      updatedAt
-      navbar {
-        socialMediaIcons {
-          url
-          icon {
-            url
-          }
-        }
-        navigationLinks {
-          isExternalLink
-          title
-          url
-        }
-        callToActions {
-          callToActions {
-            icon {
-              url
-            }
-            ctaType
-            isExternalLink
-            linkUrl
-          }
-        }
-      }
-    }
-  }
-`;
 
 export async function getStaticProps(ctx: GetStaticPropsContext) {
   const currentSlug = ctx.params?.slug;
@@ -63,7 +63,6 @@ export async function getStaticPaths() {
 
   const data = await request(GET_ALL_PAGE_SLUGS_QUERY);
 
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   paths = [...paths, ...data.allPages.map((page: any) => ({ params: { slug: page.slug } }))];
 
   return {
