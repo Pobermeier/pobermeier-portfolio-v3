eef75a21d09f0adecacf55ed0b7c4bb1c896748b
Patrick Obermeier <pato@xxxlgroup.com>
2022-01-09 18:43:27 +0100

    create SocialMediaIcon-component

---
+++ src/components/SocialMedia/SocialMediaIcon.tsx
@@ -0,0 +1,19 @@
+/* eslint-disable @next/next/no-img-element */
+import React from "react";
+
+interface Props {
+  name: string;
+  url: string;
+  iconSrc: string;
+}
+
+const SocialMediaIcon = ({ iconSrc, name, url }: Props) => {
+  return (
+    <a href={url} className="text-gray-400 hover:text-gray-500" title={name}>
+      <span className="sr-only">{name}</span>
+      <img className="h-6 w-6" aria-hidden="true" alt={name} src={iconSrc} />
+    </a>
+  );
+};
+
+export default SocialMediaIcon;
