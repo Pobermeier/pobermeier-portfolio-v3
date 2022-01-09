5ebb3aa98f29b159d33127d7b42c51c998c837e1
Patrick Obermeier <pato@xxxlgroup.com>
2022-01-09 22:13:43 +0100

    move getPageData-query to separate file

---
+++ src/graphql/queries/getPageData.ts
@@ -0,0 +1,145 @@
+import { gql } from "graphql-request";
+
+export const GET_PAGE_DATA_QUERY = gql`
+  query GetPageData($slug: String!) {
+    page(filter: { slug: { eq: $slug } }) {
+      title
+      slug
+      navbar {
+        socialMediaIcons {
+          id
+          url
+          name
+          icon {
+            url
+          }
+        }
+        navigationLinks {
+          id
+          isExternalLink
+          title
+          url
+        }
+        callToActions {
+          callToActions {
+            id
+            icon {
+              url
+            }
+            ctaType
+            isExternalLink
+            linkUrl
+            title
+          }
+        }
+      }
+      sections {
+        __typename
+        ... on HeroRecord {
+          id
+          heading
+          subHeading
+          backgroundImage {
+            url
+          }
+          callToActions {
+            callToActions {
+              id
+              icon {
+                url
+              }
+              ctaType
+              isExternalLink
+              linkUrl
+            }
+          }
+        }
+        ... on StaticContentBlockRecord {
+          id
+          title
+          text
+        }
+        ... on SectionRecord {
+          id
+          headline
+          description
+          reactComponent
+          dynamicContent {
+            __typename
+            ... on CtaContainerRecord {
+              callToActions {
+                id
+                icon {
+                  url
+                }
+                ctaType
+                isExternalLink
+                linkUrl
+              }
+            }
+            ... on LogoSliderRecord {
+              title
+              logos {
+                id
+                name
+                logo {
+                  url
+                }
+              }
+            }
+            ... on ProjectGridRecord {
+              id
+              projects {
+                id
+                title
+                subTitle
+                description
+                projectType
+                liveLink
+                repoLink
+                showcaseImage {
+                  url
+                }
+                projectLogo {
+                  url
+                }
+                technologiesUsed {
+                  id
+                  name
+                  logo {
+                    url
+                  }
+                }
+              }
+            }
+            ... on TextWithImageRecord {
+              id
+              text
+              image {
+                title
+                url
+              }
+            }
+          }
+        }
+      }
+      footer {
+        copyrightText
+        navigationLinks {
+          id
+          title
+          isExternalLink
+          url
+        }
+        socialMediaIcons {
+          id
+          url
+          name
+          icon {
+            url
+          }
+        }
+      }
+    }
+  }
+`;
