c6d5a9d38bea2ab2754f253cb87ec2bdd9727684
Patrick Obermeier <pato@xxxlgroup.com>
2022-01-10 17:12:00 +0100

    increase scale of social media icons on hover-state

--- src/components/SocialMedia/SocialMediaIcon.tsx
+++ src/components/SocialMedia/SocialMediaIcon.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable @next/next/no-img-element */
 import React from "react";
 
 interface Props {
@@ -8,9 +7,15 @@ interface Props {
   size?: number;
 }
 
-const SocialMediaIcon = ({ iconSrc, name, url, size = 6 }: Props) => {
+const SocialMediaIcon = ({ iconSrc, name, url, size = 5 }: Props) => {
   return (
-    <a href={url} className="text-gray-400 hover:text-gray-500" title={name}>
+    <a
+      href={url}
+      target="_blank"
+      rel="noreferrer noopener"
+      className="text-gray-400 hover:text-primary focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-800 hover:scale-110 transition-all"
+      title={name}
+    >
       <span className="sr-only">{name}</span>
       <img className={`h-${size} w-${size}`} aria-hidden="true" alt={name} src={iconSrc} />
     </a>
