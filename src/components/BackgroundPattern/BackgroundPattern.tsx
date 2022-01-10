c638c7bfa36ecd88be34f265ab87c7318cb43066
Patrick Obermeier <pato@xxxlgroup.com>
2022-01-10 17:01:31 +0100

    create BackgroundPattern-component

---
+++ src/components/BackgroundPattern/BackgroundPattern.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+interface Props {
+  positionClassName: string;
+}
+
+const BackgroundPattern = ({ positionClassName }: Props) => {
+  return (
+    <svg
+      className={`absolute ${positionClassName}`}
+      width={404}
+      height={384}
+      fill="none"
+      viewBox="0 0 404 384"
+    >
+      <defs>
+        <pattern
+          id="74b3fd99-0a6f-4271-bef2-e80eeafdf357"
+          x={0}
+          y={0}
+          width={20}
+          height={20}
+          patternUnits="userSpaceOnUse"
+        >
+          <rect x={0} y={0} width={4} height={4} className="text-green-200" fill="currentColor" />
+        </pattern>
+      </defs>
+      <rect width={404} height={384} fill="url(#74b3fd99-0a6f-4271-bef2-e80eeafdf357)" />
+    </svg>
+  );
+};
+
+export default BackgroundPattern;
