bc0d6fd99c4225f6d8450d3e80058f2203698343
Patrick Obermeier <pato@xxxlgroup.com>
2022-01-10 17:11:24 +0100

    create StaticContentBlock-component

---
+++ src/components/StaticContentBlock/StaticContentBlock.tsx
@@ -0,0 +1,34 @@
+import BackgroundPattern from "components/BackgroundPattern/BackgroundPattern";
+import BackgroundPatternWrapper from "components/BackgroundPattern/BackgroundPatternWrapper";
+import React from "react";
+import ReactMarkdown from "react-markdown";
+
+export type StaticContentBlockRecordProps = {
+  title: string;
+  text: string;
+};
+
+const StaticContentBlock = ({ title, text }: StaticContentBlockRecordProps) => {
+  return (
+    <div className="relative pt-16 overflow-hidden">
+      <BackgroundPatternWrapper>
+        <BackgroundPattern positionClassName="top-12 left-full transform translate-x-32" />
+      </BackgroundPatternWrapper>
+      <div className="max-w-4xl relative px-4 sm:px-6 lg:px-8 mx-auto">
+        <div className="text-lg mx-auto">
+          <h1 className="mt-2 block text-3xl text-left leading-8 font-bold tracking-tight text-primary sm:text-4xl">
+            {title}
+          </h1>
+        </div>
+        <div className="mt-6 prose prose-indigo prose-lg text-gray-500 mx-auto max-w-4xl prose-a:text-primary prose-a:no-underline">
+          <ReactMarkdown>{text}</ReactMarkdown>
+        </div>
+      </div>
+      <BackgroundPatternWrapper>
+        <BackgroundPattern positionClassName="bottom-12 right-full transform -translate-x-32" />
+      </BackgroundPatternWrapper>
+    </div>
+  );
+};
+
+export default StaticContentBlock;
