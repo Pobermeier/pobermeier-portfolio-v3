da618c93d918f660137fa4193f2d7a2d716878f5
Patrick Obermeier <pato@xxxlgroup.com>
2022-01-10 17:00:55 +0100

    configure tailwind-typography plugin & add custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,10 +5,13 @@ module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      colors: {
+        primary: "#239a3b",
+      },
       fontFamily: {
         sans: ["Inter var", ...defaultTheme.fontFamily.sans],
       },
     },
   },
-  plugins: [],
+  plugins: [require("@tailwindcss/typography")],
 };
