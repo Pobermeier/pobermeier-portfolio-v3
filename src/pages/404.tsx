ef2ab8d4d1f8bd8912f292eb07035ae23be9f17d
Patrick Obermeier <pato@xxxlgroup.com>
2022-01-10 17:08:15 +0100

    add content to 404-page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,10 +1,34 @@
-import SEO from "@/components/SEO/SEO";
+import Button from "components/Buttons/Button";
+import SiteLogoLinked from "components/Logos/SiteLogoLinked";
+import SEO from "components/SEO/SEO";
 
 const NotFoundPage = () => {
   return (
     <>
       <SEO title="Not Found | Patrick Obermeier" />
-      <h1>404 Not Found</h1>
+      <div className="min-h-screen pt-16 pb-12 flex flex-col bg-gray-50">
+        <main className="flex-grow flex flex-col justify-center max-w-7xl w-full mx-auto px-4 sm:px-6 lg:px-8">
+          <div className="flex-shrink-0 flex justify-center">
+            <SiteLogoLinked />
+          </div>
+          <div className="py-16">
+            <div className="text-center">
+              <p className="text-sm font-semibold text-primary uppercase tracking-wide">
+                404 Page Not Found
+              </p>
+              <h1 className="mt-2 text-4xl font-extrabold text-gray-900 tracking-tight sm:text-5xl">
+                Oh noes! A big ol&apos; 404!
+              </h1>
+              <p className="mt-2 text-base text-gray-500">
+                This is not the page you&apos;re looking for...
+              </p>
+              <div className="mt-6">
+                <Button text="Back to Homepage" as="a" size="lg" url="/" />
+              </div>
+            </div>
+          </div>
+        </main>
+      </div>
     </>
   );
 };
