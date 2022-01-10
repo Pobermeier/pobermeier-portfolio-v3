25e9571b48c01af56da3d06c041d89336946d3bf
Patrick Obermeier <pato@xxxlgroup.com>
2022-01-10 17:08:45 +0100

    update styling of navItem-component

--- src/components/Navigation/NavItem.tsx
+++ src/components/Navigation/NavItem.tsx
@@ -7,16 +7,19 @@ interface Props {
   url: string;
 }
 
+const linkClassName =
+  "text-base font-medium text-gray-500 hover:text-primary focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-800";
+
 const NavItem = ({ isExternalLink, name, url }: Props) => {
   return (
     <>
       {isExternalLink ? (
-        <a href={url} className="text-base font-medium text-gray-500 hover:text-green-600">
+        <a href={url} className={linkClassName} target="_blank" rel="noreferrer noopener">
           {name}
         </a>
       ) : (
         <Link href={url}>
-          <a className="text-base font-medium text-gray-500 hover:text-green-600">{name}</a>
+          <a className={linkClassName}>{name}</a>
         </Link>
       )}
     </>
