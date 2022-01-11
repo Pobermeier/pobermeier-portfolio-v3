9e8006002c00c428abebde59f864eca2464576e0
Patrick Obermeier <pato@xxxlgroup.com>
2022-01-11 13:43:58 +0100

    create custom usePreviewMode-hook

---
+++ src/hooks/usePreviewMode.ts
@@ -0,0 +1,23 @@
+import { Dispatch, SetStateAction, useEffect, useState } from "react";
+import { PREVIEW_STORAGE_ITEM_NAME } from "Constants";
+
+const usePreviewMode = (): [boolean, Dispatch<SetStateAction<boolean>>] => {
+  const [isPreview, setIsPreview] = useState(false);
+
+  useEffect(() => {
+    const isPreviewFlagSetInLocalStorage =
+      Boolean(localStorage.getItem(PREVIEW_STORAGE_ITEM_NAME)) || false;
+
+    setIsPreview(isPreviewFlagSetInLocalStorage);
+  }, []);
+
+  useEffect(() => {
+    isPreview
+      ? localStorage.setItem(PREVIEW_STORAGE_ITEM_NAME, isPreview.toString())
+      : localStorage.removeItem(PREVIEW_STORAGE_ITEM_NAME);
+  }, [isPreview]);
+
+  return [isPreview, setIsPreview];
+};
+
+export default usePreviewMode;
