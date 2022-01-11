b81796ee663a4ea0e908664c699ab5795846b57d
Patrick Obermeier <pato@xxxlgroup.com>
2022-01-11 13:45:23 +0100

    implement first iteration of live preview mode

--- src/pages/[slug].tsx
+++ src/pages/[slug].tsx
@@ -1,24 +1,44 @@
 import { GetStaticPropsContext } from "next";
+import { useQuerySubscription } from "react-datocms";
 
 import Navbar, { NavbarProps } from "components/Navigation/Navbar";
 import Footer, { FooterProps } from "components/Footer/Footer";
 import CmsComponentMapper from "components/CMS/CmsComponentMapper";
+import PreviewBanner from "components/Banner/PreviewBanner";
 import SEO from "components/SEO/SEO";
 import { request } from "clients/datocms";
 import { CmsData } from "models/datoCMS";
 import { GET_PAGE_DATA_QUERY } from "graphql/queries/getPageData";
 import { GET_ALL_PAGE_SLUGS_QUERY } from "graphql/queries/getAllPageSlugs";
+import { isDev, PREVIEW_STORAGE_ITEM_NAME } from "Constants";
 import config from "config";
 
-interface Props {
+type InternalProps = {
   data: CmsData;
-}
+};
 
-const Page = ({
-  data: {
+type ExternalProps = {
+  isPreview: boolean;
+  deactivatePreviewMode: () => void;
+};
+
+type Props = InternalProps & ExternalProps;
+
+const Page = ({ data, isPreview, deactivatePreviewMode }: Props) => {
+  const { data: cmsData } = useQuerySubscription({
+    enabled:
+      typeof window !== "undefined" && Boolean(localStorage.getItem(PREVIEW_STORAGE_ITEM_NAME)),
+    query: GET_PAGE_DATA_QUERY,
+    variables: { slug: data.page.slug },
+    initialData: data,
+    preview: true,
+    token: process.env.NEXT_PUBLIC_DATOCMS_API_TOKEN as string,
+  });
+
+  const {
     page: { navbar, footer, sections, title, slug },
-  },
-}: Props) => {
+  } = cmsData as CmsData;
+
   const computedTitle = `${title} | ${config.meta.siteOwnerName}`;
   const homeRouteTitle = `Senior Frontend Developer | ${config.meta.siteOwnerName}`;
 
@@ -51,6 +71,7 @@ const Page = ({
   return (
     <>
       <SEO title={isHomeRoute ? homeRouteTitle : computedTitle} />
+      {isPreview && <PreviewBanner onLeavePreviewBtnClick={deactivatePreviewMode} />}
       {navbar && renderNavbar()}
       <main>
         {sections?.map(({ __typename, id, ...other }) => (
@@ -65,7 +86,7 @@ const Page = ({
 export async function getStaticProps(ctx: GetStaticPropsContext) {
   const currentSlug = ctx.params?.slug;
 
-  const data = await request(GET_PAGE_DATA_QUERY, { slug: currentSlug });
+  const data = await request(GET_PAGE_DATA_QUERY, { slug: currentSlug }, isDev);
 
   return {
     props: { data },
@@ -73,7 +94,7 @@ export async function getStaticProps(ctx: GetStaticPropsContext) {
 }
 
 export async function getStaticPaths() {
-  const cmsData = await request(GET_ALL_PAGE_SLUGS_QUERY);
+  const cmsData = await request(GET_ALL_PAGE_SLUGS_QUERY, null, isDev);
 
   const paths = cmsData.allPages.map((page: any) => ({ params: { slug: page.slug } }));
 
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,14 +2,25 @@ import type { AppProps } from "next/app";
 
 import useCookieBanner from "hooks/useCookieBanner";
 import useGoogleTagManager from "hooks/useGoogleTagManager";
+import useQueryCheck from "hooks/useQueryCheck";
+import usePreviewMode from "hooks/usePreviewMode";
 
 import "styles/globals.css";
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
-  useCookieBanner();
+  const [isPreview, setIsPreview] = usePreviewMode();
+  useQueryCheck("isPreview", setIsPreview);
   useGoogleTagManager();
+  useCookieBanner();
 
-  return <Component {...pageProps} />;
+  const deactivatePreviewMode = () => {
+    setIsPreview(false);
+    window.location = (window.location.pathname + window.location.hash) as unknown as Location;
+  };
+
+  return (
+    <Component {...pageProps} isPreview={isPreview} deactivatePreviewMode={deactivatePreviewMode} />
+  );
 };
 
 export default MyApp;
