1df69083bdff480d52a6153a3cf4eb38749dacd5
Patrick Obermeier <pato@xxxlgroup.com>
2022-01-11 22:34:55 +0100

    remove meta tags that will be filled with cms data

--- src/components/SEO/SEO.tsx
+++ src/components/SEO/SEO.tsx
@@ -3,57 +3,29 @@ import { useRouter } from "next/dist/client/router";
 
 import config from "config";
 
-const SEO: React.FC<Props> = ({ title, description, keywords, socialShareImgUrl, siteName }) => {
+const SEO: React.FC<Props> = ({ children, keywords }) => {
   const router = useRouter();
 
   return (
     <Head>
       {/* <!-- General Meta-Tags & SEO--> */}
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <title>{title}</title>
-      <meta name="description" content={description} />
+      {children}
       <meta name="author" content={config.meta.siteOwnerName} />
       <meta name="keywords" content={keywords} />
       <meta name="theme-color" content={config.meta.themeColor} />
       <link rel="canonical" href={`${config.meta.canonicalRootUrl}${router.asPath}`} />
       <meta name="msvalidate.01" content={config.meta.bingValidationId} />
-
-      {/* <!-- Social Media Stuff / Sharing --> */}
-      {/* <!-- Open Graph (Facebook) --> */}
-      <meta property="og:url" content={`${config.meta.canonicalRootUrl}${router.asPath}`} />
-      <meta property="og:type" content="website" />
-      <meta property="og:title" content={title} />
-      <meta property="og:image" content={socialShareImgUrl} />
-      <meta property="og:description" content={description} />
-      <meta property="og:site_name" content={siteName} />
-      <meta property="article:author" content={config.meta.siteOwnerName} />
-
-      {/* <!-- Twitter Cards --> */}
-      <meta name="twitter:card" content="summary_large_image" />
-      <meta name="twitter:url" content={`${config.meta.canonicalRootUrl}${router.asPath}`} />
-      <meta name="twitter:title" content={title} />
-      <meta name="twitter:site" content={config.meta.twitterHandle} />
-      <meta name="twitter:creator" content={config.meta.twitterHandle} />
-      <meta name="twitter:description" content={description} />
-      <meta property="og:image" content={socialShareImgUrl} />
     </Head>
   );
 };
 
 SEO.defaultProps = {
-  title: config.meta.defaultTitle,
-  description: config.meta.defaultDescription,
   keywords: config.meta.defaultKeywords,
-  socialShareImgUrl: config.meta.socialShareImgUrl,
-  siteName: config.meta.defaultTitle,
 };
 
 interface Props {
-  title?: string;
-  description?: string;
   keywords?: string;
-  socialShareImgUrl?: string;
-  siteName?: string;
 }
 
 export default SEO;
