61ca9c7304206647281875cf7af42fb6eb6136ef
Patrick Obermeier <pato@xxxlgroup.com>
2022-01-13 00:41:07 +0100

    update theme colors & configure dark mode in tailwind-config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,21 @@
 const defaultTheme = require("tailwindcss/defaultTheme");
+const colors = require("tailwindcss/colors");
 
 module.exports = {
+  darkMode: "class",
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {
-      colors: {
-        primary: "#239a3b",
-      },
-      fontFamily: {
-        sans: ["Inter var", ...defaultTheme.fontFamily.sans],
-      },
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      primary: "#239a3b",
+      black: colors.black,
+      white: colors.white,
+      gray: colors.gray,
+      green: colors.green,
+    },
+    fontFamily: {
+      sans: ["Inter var", ...defaultTheme.fontFamily.sans],
     },
   },
   plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms")],
