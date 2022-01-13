0c037473632e927e155e80fe6821faedade2455e
Patrick Obermeier <pato@xxxlgroup.com>
2022-01-14 00:05:45 +0100

    decrease code repetition by refactoring repeating classnames into object

--- src/components/Contact/ContactForm.tsx
+++ src/components/Contact/ContactForm.tsx
@@ -6,6 +6,13 @@ interface Props {
   ctaButtonText: string;
 }
 
+const classNames = {
+  form: "grid grid-cols-1 gap-y-6 sm:grid-cols-2 sm:gap-x-8 max-w-2xl mx-auto",
+  label: "block text-sm font-medium text-gray-700 dark:text-gray-400",
+  input:
+    "py-3 px-4 block w-full shadow-sm focus:ring-green-800 focus:border-green-800 border-gray-300 rounded-md",
+};
+
 const ContactForm = ({ ctaButtonText, id }: Props) => {
   return (
     <form
@@ -14,13 +21,10 @@ const ContactForm = ({ ctaButtonText, id }: Props) => {
       id={`contact-form-${id}`}
       name={`contact-form-${id}`}
       method="POST"
-      className="grid grid-cols-1 gap-y-6 sm:grid-cols-2 sm:gap-x-8 max-w-2xl mx-auto"
+      className={classNames.form}
     >
       <div>
-        <label
-          htmlFor="first-name"
-          className="block text-sm font-medium text-gray-700 dark:text-gray-400"
-        >
+        <label htmlFor="first-name" className={classNames.label}>
           First Name
         </label>
         <div className="mt-1">
@@ -30,15 +34,12 @@ const ContactForm = ({ ctaButtonText, id }: Props) => {
             id="first-name"
             autoComplete="given-name"
             required
-            className="py-3 px-4 block w-full shadow-sm focus:ring-green-800 focus:border-green-800 border-gray-300 rounded-md"
+            className={classNames.input}
           />
         </div>
       </div>
       <div>
-        <label
-          htmlFor="last-name"
-          className="block text-sm font-medium text-gray-700 dark:text-gray-400"
-        >
+        <label htmlFor="last-name" className={classNames.label}>
           Last Name
         </label>
         <div className="mt-1">
@@ -48,15 +49,12 @@ const ContactForm = ({ ctaButtonText, id }: Props) => {
             id="last-name"
             autoComplete="family-name"
             required
-            className="py-3 px-4 block w-full shadow-sm focus:ring-green-800 focus:border-green-800 border-gray-300 rounded-md"
+            className={classNames.input}
           />
         </div>
       </div>
       <div className="sm:col-span-2">
-        <label
-          htmlFor="email"
-          className="block text-sm font-medium text-gray-700 dark:text-gray-400"
-        >
+        <label htmlFor="email" className={classNames.label}>
           Email
         </label>
         <div className="mt-1">
@@ -66,32 +64,20 @@ const ContactForm = ({ ctaButtonText, id }: Props) => {
             type="email"
             autoComplete="email"
             required
-            className="py-3 px-4 block w-full shadow-sm focus:ring-green-800 focus:border-green-800 border-gray-300 rounded-md"
+            className={classNames.input}
           />
         </div>
       </div>
       <div className="sm:col-span-2">
-        <label
-          htmlFor="subject"
-          className="block text-sm font-medium text-gray-700 dark:text-gray-400"
-        >
+        <label htmlFor="subject" className={classNames.label}>
           Subject
         </label>
         <div className="mt-1">
-          <input
-            type="text"
-            name="subject"
-            id="subject"
-            required
-            className="py-3 px-4 block w-full shadow-sm focus:ring-green-800 focus:border-green-800 border-gray-300 rounded-md"
-          />
+          <input type="text" name="subject" id="subject" required className={classNames.input} />
         </div>
       </div>
       <div className="sm:col-span-2">
-        <label
-          htmlFor="message"
-          className="block text-sm font-medium text-gray-700 dark:text-gray-400"
-        >
+        <label htmlFor="message" className={classNames.label}>
           Message
         </label>
         <div className="mt-1">
@@ -100,7 +86,7 @@ const ContactForm = ({ ctaButtonText, id }: Props) => {
             name="message"
             rows={4}
             required
-            className="py-3 px-4 block w-full shadow-sm focus:ring-green-800 focus:border-green-800 border border-gray-300 rounded-md"
+            className={classNames.input}
             defaultValue={""}
           />
         </div>
