ca659b2df426afa9a8d26843f75dd5231ea26409
Patrick Obermeier <pato@xxxlgroup.com>
2022-01-13 16:29:23 +0100

    use reusable Banner-component inside PreviewBanner

--- src/components/Banner/PreviewBanner.tsx
+++ src/components/Banner/PreviewBanner.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Button from "components/Buttons/Button";
+import Banner from "./Banner";
 
 interface Props {
   onLeavePreviewBtnClick: () => void;
@@ -7,24 +8,18 @@ interface Props {
 
 const PreviewBanner = ({ onLeavePreviewBtnClick }: Props) => {
   return (
-    <div className="bg-primary">
-      <div className="max-w-7xl mx-auto py-3 px-4 sm:px-6">
-        <div className="flex-column sm:flex items-center justify-between flex-wrap">
-          <div className="flex-1 flex items-center justify-center sm:justify-start">
-            <p className="ml-3 font-medium text-white">Live Preview Mode is currently active!</p>
-          </div>
-          <div className="mt-2 flex-shrink-0 w-full sm:mt-0 sm:w-auto">
-            <Button
-              type="secondary"
-              text="Leave Preview Mode"
-              size="xs"
-              isFullWidth
-              onClick={onLeavePreviewBtnClick}
-            />
-          </div>
-        </div>
-      </div>
-    </div>
+    <Banner
+      text="Live Preview Mode is currently active!"
+      ctaButtons={
+        <Button
+          type="secondary"
+          text="Leave Preview Mode"
+          size="xs"
+          isFullWidth
+          onClick={onLeavePreviewBtnClick}
+        />
+      }
+    />
   );
 };
 
