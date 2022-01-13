6c7a96cb51dd5df150d782c25cd14418e0cd61f0
Patrick Obermeier <pato@xxxlgroup.com>
2022-01-13 23:40:44 +0100

    decrease max-width of ContentSection-component

--- src/components/ContentSection/ContentSection.tsx
+++ src/components/ContentSection/ContentSection.tsx
@@ -18,7 +18,7 @@ const ContentSection: React.FC<ContentSectionProps> = ({
   return (
     <div>
       <section className="bg-gray-50 dark:bg-gray-900 py-16 px-4 overflow-hidden sm:px-6 lg:px-8 lg:py-24 relative">
-        <div className="max-w-7xl mx-auto" data-aos="fade-up">
+        <div className="max-w-5xl mx-auto" data-aos="fade-up">
           <div className="text-center">
             {heading && <ContentSectionHeading headingText={heading} />}
             {description && <ContentSectionParagraph text={description} />}
