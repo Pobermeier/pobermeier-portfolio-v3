63cc7afccc4c84dc07352cc164235d1906c2a86f
Patrick Obermeier <pato@xxxlgroup.com>
2022-01-13 16:26:51 +0100

    create DarkIcon-component

---
+++ src/components/Icons/DarkIcon.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const DarkIcon = () => {
+  return (
+    <svg
+      className="w-5 h-5"
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth={2}
+        d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
+      />
+    </svg>
+  );
+};
+
+export default DarkIcon;
