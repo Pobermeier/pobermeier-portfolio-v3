40d12c7e84a6d8f4e6b9a58e021ae78d3a95d8e3
Patrick Obermeier <pato@xxxlgroup.com>
2022-01-13 22:07:59 +0100

    exclude decoration elemens in ContentSection from fade-up animation

--- src/components/ContentSection/ContentSection.tsx
+++ src/components/ContentSection/ContentSection.tsx
@@ -17,11 +17,8 @@ const ContentSection: React.FC<Props> = ({
 }) => {
   return (
     <div>
-      <section
-        className="bg-gray-50 dark:bg-gray-900 py-16 px-4 overflow-hidden sm:px-6 lg:px-8 lg:py-24 relative"
-        data-aos="fade-up"
-      >
-        <div className="max-w-7xl mx-auto">
+      <section className="bg-gray-50 dark:bg-gray-900 py-16 px-4 overflow-hidden sm:px-6 lg:px-8 lg:py-24 relative">
+        <div className="max-w-7xl mx-auto" data-aos="fade-up">
           <div className="text-center">
             {heading && <ContentSectionHeading headingText={heading} />}
             {description && <ContentSectionParagraph text={description} />}
