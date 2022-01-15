58ee24e6ef3623e2307d1023a520f8c5051efb75
Patrick Obermeier <pato@xxxlgroup.com>
2022-01-15 15:22:52 +0100

    create technology-logo component

---
+++ src/components/Logos/TechnologyLogo.tsx
@@ -0,0 +1,25 @@
+/* eslint-disable @next/next/no-img-element */
+interface Props {
+  technologyName: string;
+  imgSrc: string;
+  width: number;
+  height: number;
+}
+
+const TechnologyLogo: React.FC<Props> = ({ width, height, imgSrc, technologyName }) => {
+  return (
+    <>
+      <img
+        loading="lazy"
+        width={width}
+        height={height}
+        src={imgSrc}
+        alt={technologyName}
+        title={technologyName}
+      />
+      <span className="sr-only">{technologyName}</span>
+    </>
+  );
+};
+
+export default TechnologyLogo;
