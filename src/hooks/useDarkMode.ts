21f28ccca3de1512f90ac2268f555852d73d7e7d
Patrick Obermeier <pato@xxxlgroup.com>
2022-01-16 18:33:51 +0100

    fix issue with useDarkMode-logic

--- src/hooks/useDarkMode.ts
+++ src/hooks/useDarkMode.ts
@@ -1,25 +1,31 @@
 import { Dispatch, SetStateAction, useEffect, useState } from "react";
 
-const DEFAULT_VALUE = true;
-
 const useDarkMode = (
   localStorageKey: string,
   rootElementId: string,
   darkmodeClassname: string,
 ): [boolean, Dispatch<SetStateAction<boolean>>] => {
-  const [darkMode, setDarkMode] = useState(DEFAULT_VALUE);
+  const [darkMode, setDarkMode] = useState(true);
 
   useEffect(() => {
-    setDarkMode(Boolean(localStorage.getItem(localStorageKey)));
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+    if (typeof window === "undefined") return;
+
+    const isDarkModeSetInStorage = localStorage.getItem(localStorageKey);
+
+    if (!isDarkModeSetInStorage) {
+      setDarkMode(true);
+      return;
+    }
+
+    setDarkMode(isDarkModeSetInStorage === "true");
+  }, [localStorageKey]);
 
   useEffect(() => {
     if (darkMode) {
       localStorage.setItem(localStorageKey, "true");
       document.getElementById(rootElementId)?.classList.add(darkmodeClassname);
     } else {
-      localStorage.removeItem(localStorageKey);
+      localStorage.setItem(localStorageKey, "false");
       document.getElementById(rootElementId)?.classList.remove(darkmodeClassname);
     }
   }, [darkMode, darkmodeClassname, localStorageKey, rootElementId]);
