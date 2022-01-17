971de5d9738877d62f46c78a893f1a921139ea70
Patrick Obermeier <pato@xxxlgroup.com>
2022-01-17 19:44:28 +0100

    move gtm-script from _document to _app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,20 @@
 import dynamic from "next/dynamic";
 import type { AppProps } from "next/app";
-
+import Script from "next/script";
+// constants
+import { isProd } from "Constants";
+// hooks
 import useCookieBanner from "hooks/useCookieBanner";
 import useGoogleTagManager from "hooks/useGoogleTagManager";
 import useQueryCheck from "hooks/useQueryCheck";
+import useScrollToHashOnPageLoad from "hooks/useScrolltoHashOnPageLoad";
 import usePreviewMode from "hooks/usePreviewMode";
 import useAOS from "hooks/useAOS";
-
+// styles
 import "styles/globals.css";
 import "styles/inter.css";
-import useScrollToHashOnPageLoad from "hooks/useScrolltoHashOnPageLoad";
+// config
+import config from "config";
 
 const AOSStyles = dynamic(() => import("components/AOS/AOSStyles"));
 
@@ -38,6 +43,17 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
         isPreview={isPreview}
         deactivatePreviewMode={deactivatePreviewMode}
       />
+      {/* Google Tag Manager */}
+      {isProd && (
+        <Script id="gtm" strategy="afterInteractive">
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','${config.gtm.id}');`}
+        </Script>
+      )}
+      {/* End Google Tag Manager */}
     </>
   );
 };
