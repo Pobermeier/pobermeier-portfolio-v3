df0231bf168eb1395c4ca9d67216f75c48e173b6
Patrick Obermeier <pato@xxxlgroup.com>
2022-01-17 19:50:00 +0100

    slightly decreased vertical spacing betweec buttons on mobile sizes

--- src/components/Buttons/CTAContainer.tsx
+++ src/components/Buttons/CTAContainer.tsx
@@ -9,7 +9,7 @@ export interface CTAContainerProps {
 
 const CTAContainer = ({ callToActions }: CTAContainerProps) => {
   return (
-    <div className="my-16 flex flex-col sm:flex-row justify-center items-center space-y-6 sm:space-x-8 sm:space-y-0">
+    <div className="my-16 flex flex-col sm:flex-row justify-center items-center space-y-4 sm:space-x-8 sm:space-y-0">
       {callToActions?.map(({ ctaType, icon, id, isExternalLink, linkUrl, title }) => (
         <Button
           key={id}
