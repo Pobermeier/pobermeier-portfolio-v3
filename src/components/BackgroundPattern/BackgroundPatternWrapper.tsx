ef5256a58a540b82c2a199c382cb4ac852c9b429
Patrick Obermeier <pato@xxxlgroup.com>
2022-01-22 18:10:16 +0100

    update z-index on BackgroundPatternWrapper

--- src/components/BackgroundPattern/BackgroundPatternWrapper.tsx
+++ src/components/BackgroundPattern/BackgroundPatternWrapper.tsx
@@ -1,6 +1,6 @@
 const BackgroundPatternWrapper: React.FC = ({ children }) => {
   return (
-    <div className="hidden lg:block lg:absolute lg:inset-y-0 lg:h-full lg:w-full -z pointer-events-none">
+    <div className="hidden lg:block lg:absolute lg:inset-y-0 lg:h-full lg:w-full -z-9 pointer-events-none">
       <div className="relative h-full text-lg max-w-prose mx-auto" aria-hidden="true">
         {children}
       </div>
