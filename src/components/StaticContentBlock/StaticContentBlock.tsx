c5ac70ebcb06c978f7d222ddb61996967ad72d8f
Patrick Obermeier <pato@xxxlgroup.com>
2022-01-23 15:07:01 +0100

    make title-prop optional

--- src/components/StaticContentBlock/StaticContentBlock.tsx
+++ src/components/StaticContentBlock/StaticContentBlock.tsx
@@ -4,7 +4,7 @@ import BackgroundPattern from "components/BackgroundPattern/BackgroundPattern";
 import BackgroundPatternWrapper from "components/BackgroundPattern/BackgroundPatternWrapper";
 
 export type StaticContentBlockRecordProps = {
-  title: string;
+  title?: string;
   text: string;
 };
 
@@ -15,9 +15,11 @@ const StaticContentBlock = ({ title, text }: StaticContentBlockRecordProps) => {
         <BackgroundPattern positionClassName="top-24 left-full transform translate-x-40 rotate-45" />
       </BackgroundPatternWrapper>
       <div className="max-w-4xl relative px-4 sm:px-6 lg:px-8 mx-auto" data-aos="fade-up">
-        <h2 className="mt-2 block text-3xl text-left leading-8 font-bold tracking-tight text-primary sm:text-4xl">
-          {title}
-        </h2>
+        {title && (
+          <h2 className="mt-2 block text-3xl text-left leading-8 font-bold tracking-tight text-primary sm:text-4xl">
+            {title}
+          </h2>
+        )}
         <div className="mt-6 prose prose-indigo prose-lg text-gray-600 dark:text-gray-100 mx-auto max-w-4xl prose-a:text-primary dark:prose-strong:text-gray-400 prose-headings:text-primary">
           <ReactMarkdown>{text}</ReactMarkdown>
         </div>
