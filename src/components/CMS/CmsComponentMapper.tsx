617a0dee0f3723aa1ead4d45db57ceb6c7ed5fe5
Patrick Obermeier <pato@xxxlgroup.com>
2022-01-23 12:47:11 +0100

    simplified logic of CmsComponentMapper

--- src/components/CMS/CmsComponentMapper.tsx
+++ src/components/CMS/CmsComponentMapper.tsx
@@ -1,16 +1,6 @@
 import dynamic from "next/dynamic";
 // types
 import { CmsComponent, CmsComponentName } from "models/datoCMS";
-import { ContactSectionProps } from "components/Contact/ContactSection";
-import { FullscreenPdfViewerProps } from "components/FullscreenPdfViewer/FullscreenPdfViewer";
-import { HeroSectionProps } from "components/Hero/HeroSection";
-import { StaticContentBlockRecordProps } from "components/StaticContentBlock/StaticContentBlock";
-import { LogoSliderProps } from "components/LogoSlider/LogoSlider";
-import { ContentSectionProps } from "components/ContentSection/ContentSection";
-import { TextWithImageProps } from "components/TextWithImage/TextWithImage";
-import { CTAContainerProps } from "components/Buttons/CTAContainer";
-import { ProjectGridProps } from "components/Project/ProjectGrid";
-import { BlogSectionProps } from "components/Blog/BlogSection";
 // components
 const FullscreenPdfViewer = dynamic(
   () => import("components/FullscreenPdfViewer/FullscreenPdfViewer"),
@@ -32,94 +22,27 @@ interface Props {
   componentProps: CmsComponent;
 }
 
+const componentMap = {
+  StaticContentBlockRecord: StaticContentBlock,
+  HeroRecord: HeroSection,
+  FullscreenPdfViewerRecord: FullscreenPdfViewer,
+  SectionRecord: ContentSection,
+  LogoSliderRecord: LogoSlider,
+  TextWithImageRecord: TextWithImage,
+  CtaContainerRecord: CTAContainer,
+  ProjectGridRecord: ProjectGrid,
+  BlogSectionRecord: BlogSection,
+  ContactFormRecord: ContactSection,
+};
+
 const CmsComponentMapper = ({ typeName, componentProps }: Props) => {
-  switch (true) {
-    case typeName === "StaticContentBlockRecord":
-      const { title, text } = componentProps as StaticContentBlockRecordProps;
-
-      return <StaticContentBlock title={title} text={text} />;
-
-    case typeName === "HeroRecord":
-      const { backgroundImage, callToActions, heading, subHeading } =
-        componentProps as HeroSectionProps;
-      return (
-        <HeroSection
-          heading={heading}
-          subHeading={subHeading}
-          backgroundImage={backgroundImage}
-          callToActions={callToActions}
-        />
-      );
-
-    case typeName === "FullscreenPdfViewerRecord":
-      const { pdfFile, pdfTitle } = componentProps as FullscreenPdfViewerProps;
-      return <FullscreenPdfViewer pdfFile={pdfFile} pdfTitle={pdfTitle} />;
-
-    case typeName === "SectionRecord":
-      const {
-        description: descriptionText,
-        heading: headingText,
-        dynamicContent,
-        htmlId,
-      } = componentProps as ContentSectionProps;
-      return (
-        <ContentSection
-          heading={headingText}
-          description={descriptionText}
-          dynamicContent={dynamicContent}
-          htmlId={htmlId}
-        />
-      );
-
-    case typeName === "LogoSliderRecord":
-      const { logos, title: logoSliderTitle } = componentProps as LogoSliderProps;
-      return <LogoSlider logos={logos} title={logoSliderTitle} />;
-
-    case typeName === "TextWithImageRecord":
-      const { image, text: sideText } = componentProps as TextWithImageProps;
-      return <TextWithImage image={image} text={sideText} />;
-
-    case typeName === "CtaContainerRecord":
-      const { callToActions: ctaCollection } = componentProps as CTAContainerProps;
-      return <CTAContainer callToActions={ctaCollection} />;
-
-    case typeName === "ProjectGridRecord":
-      const { projects } = componentProps as ProjectGridProps;
-      return <ProjectGrid projects={projects} />;
-
-    case typeName === "BlogSectionRecord":
-      const {
-        posts,
-        description: blogSectionDescription,
-        heading: blogSectionHeading,
-      } = componentProps as BlogSectionProps;
-      return (
-        <BlogSection
-          posts={posts}
-          description={blogSectionDescription}
-          heading={blogSectionHeading}
-        />
-      );
-
-    case typeName === "ContactFormRecord":
-      const {
-        ctaButtonText: callToAction,
-        description,
-        heading: contactFormheading,
-        id,
-      } = componentProps as ContactSectionProps;
-      return (
-        <ContactSection
-          id={id}
-          ctaButtonText={callToAction}
-          description={description}
-          heading={contactFormheading}
-        />
-      );
-
-    default:
-      return null;
+  if (typeName && componentMap[typeName]) {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const Component = componentMap[typeName] as any;
+    return <Component {...componentProps} />;
   }
+
+  return null;
 };
 
 export default CmsComponentMapper;
