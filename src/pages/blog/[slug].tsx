26d215fc88d500917886f6b8b8b04b0ddb3c706a
Patrick Obermeier <pato@xxxlgroup.com>
2022-01-23 15:47:24 +0100

    add "Go Back"-button at top of blog post

--- src/pages/blog/[slug].tsx
+++ src/pages/blog/[slug].tsx
@@ -1,28 +1,31 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
 import { GetStaticPropsContext } from "next";
 import dynamic from "next/dynamic";
 import { useQuerySubscription, renderMetaTags } from "react-datocms";
 // types
-import { CmsData, PageData } from "models/datoCMS";
-import { NavbarProps } from "components/Navigation/Navbar";
-import { FooterProps } from "components/Footer/Footer";
+import { BlogPost as BlogPostType, SiteData } from "models/datoCMS";
 // constants
 import { isDev, PREVIEW_STORAGE_ITEM_NAME } from "Constants";
 // components
+import Layout from "components/Layout/Layout";
+import StaticContentBlock from "components/StaticContentBlock/StaticContentBlock";
 const Navbar = dynamic(() => import("components/Navigation/Navbar"));
-const CmsComponentMapper = dynamic(() => import("components/CMS/CmsComponentMapper"));
 const Footer = dynamic(() => import("components/Footer/Footer"));
-const PreviewBanner = dynamic(() => import("components/Banner/PreviewBanner"));
-import SEO from "components/SEO/SEO";
 // graphql
 import { request } from "clients/datocms";
-import { GET_PAGE_DATA_QUERY } from "graphql/queries/getPageData";
-import { GET_ALL_PAGE_SLUGS_QUERY } from "graphql/queries/getAllPageSlugs";
-// config
-import globalConfig from "config";
+import { GET_ALL_BLOG_PAGES_SLUGS } from "graphql/queries/getAllBlogSlugs";
+import { GET_BLOG_POST } from "graphql/queries/getBlogPost";
+import navbarContent from "content/navbar";
+// content
+import footerContent from "content/footer";
+import Button from "components/Buttons/Button";
+
+type PostData = {
+  site: SiteData;
+  blogPost: BlogPostType;
+};
 
 type InternalProps = {
-  data: CmsData;
+  data: PostData;
 };
 
 type ExternalProps = {
@@ -32,12 +35,12 @@ type ExternalProps = {
 
 type Props = InternalProps & ExternalProps;
 
-const Page = ({ data, isPreview, deactivatePreviewMode }: Props) => {
-  const { data: cmsData } = useQuerySubscription({
+const BlogPost = ({ data, isPreview, deactivatePreviewMode }: Props) => {
+  const { data: postData } = useQuerySubscription({
     enabled:
       typeof window !== "undefined" && Boolean(localStorage.getItem(PREVIEW_STORAGE_ITEM_NAME)),
-    query: GET_PAGE_DATA_QUERY,
-    variables: { slug: data.page.slug },
+    query: GET_BLOG_POST,
+    variables: { slug: data.blogPost.slug },
     initialData: data,
     preview: true,
     token: process.env.NEXT_PUBLIC_DATOCMS_API_TOKEN as string,
@@ -45,73 +48,32 @@ const Page = ({ data, isPreview, deactivatePreviewMode }: Props) => {
 
   const {
     site: { favicon },
-    page: { seo, navbar, footer, sections },
-  } = cmsData as CmsData;
+    blogPost: { title, content, seo },
+  } = postData as PostData;
 
-  const renderNavbar = () => {
-    const { socialMediaIcons, navigationLinks, callToActions } = navbar as NavbarProps;
-
-    return (
-      <Navbar
-        socialMediaIcons={socialMediaIcons}
-        navigationLinks={navigationLinks}
-        callToActions={callToActions}
-      />
-    );
-  };
-
-  const renderFooter = () => {
-    const { copyrightText, navigationLinks, socialMediaIcons } = footer as FooterProps;
-
-    return (
-      <Footer
-        copyrightText={copyrightText}
-        navigationLinks={navigationLinks}
-        socialMediaIcons={socialMediaIcons}
-      />
-    );
-  };
+  const metaTags = renderMetaTags([...seo, ...favicon]);
 
   return (
-    <div className="min-h-screen flex flex-col">
-      <SEO>{renderMetaTags([...seo, ...favicon])}</SEO>
-      {isPreview && <PreviewBanner onLeavePreviewBtnClick={deactivatePreviewMode} />}
-      {navbar && renderNavbar()}
-      <main id="main" className="flex flex-col flex-grow justify-center h-full w-full mx-auto">
-        {sections?.map((section) => (
-          <CmsComponentMapper
-            key={section.id}
-            typeName={section.__typename}
-            componentProps={section}
-          />
-        ))}
-      </main>
-      {footer && renderFooter()}
-    </div>
+    <Layout
+      metaTags={metaTags}
+      isPreview={isPreview}
+      deactivatePreviewMode={deactivatePreviewMode}
+      header={<Navbar {...navbarContent} />}
+      footer={<Footer {...footerContent} />}
+    >
+      <div className="max-w-4xl w-full mx-auto px-4 sm:px-6 lg:px-8">
+        <Button as="a" text="Back to Blog Posts" type="secondary" url="/blog" />
+      </div>
+      <StaticContentBlock text={content} title={title} />
+    </Layout>
   );
 };
 
 export async function getStaticProps(ctx: GetStaticPropsContext) {
   const currentSlug = ctx.params?.slug;
 
-  if (globalConfig.flags.useMockData) {
-    const fs = require("fs");
-    const path = require("path");
-
-    const mockData = fs.readFileSync(
-      path.resolve(process.cwd(), "src", "mocks", "pages", `${currentSlug}.json`),
-      "utf8",
-    );
-
-    return {
-      props: {
-        data: JSON.parse(mockData),
-      },
-    };
-  }
-
   try {
-    const data = await request(GET_PAGE_DATA_QUERY, { slug: currentSlug }, isDev);
+    const data = await request(GET_BLOG_POST, { slug: currentSlug }, isDev);
 
     return {
       props: { data },
@@ -123,9 +85,11 @@ export async function getStaticProps(ctx: GetStaticPropsContext) {
 
 export async function getStaticPaths() {
   try {
-    const cmsData = await request(GET_ALL_PAGE_SLUGS_QUERY, null, isDev);
+    const data = await request(GET_ALL_BLOG_PAGES_SLUGS, null, isDev);
 
-    const paths = cmsData.allPages.map((page: PageData) => ({ params: { slug: page.slug } }));
+    const paths = data.allBlogPosts.map((post: { slug: string }) => ({
+      params: { slug: post.slug },
+    }));
 
     return {
       fallback: false,
@@ -136,4 +100,4 @@ export async function getStaticPaths() {
   }
 }
 
-export default Page;
+export default BlogPost;
