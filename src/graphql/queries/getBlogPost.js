ce2c5483a507cf164e15e8ede24cdfeb2f473320
Patrick Obermeier <pato@xxxlgroup.com>
2022-01-23 11:42:32 +0100

    update content of getBlogPost-query

--- src/graphql/queries/getBlogPost.js
+++ src/graphql/queries/getBlogPost.js
@@ -1,9 +1,51 @@
 const { gql } = require("graphql-request");
 
 const GET_BLOG_POST = gql`
-  query GetBlogPost {
-    allPages {
+  query GetBlogPost($slug: String!) {
+    blogPost(filter: { slug: { eq: $slug } }) {
+      author {
+        name
+        photo {
+          responsiveImage {
+            alt
+            aspectRatio
+            base64
+            bgColor
+            height
+            sizes
+            src
+            srcSet
+            title
+            webpSrcSet
+            width
+          }
+        }
+      }
+      category {
+        title
+      }
+      content
+      id
+      createdAt
+      isfeatured
+      shortDescription
       slug
+      title
+      image {
+        responsiveImage {
+          alt
+          aspectRatio
+          base64
+          bgColor
+          height
+          sizes
+          src
+          srcSet
+          title
+          webpSrcSet
+          width
+        }
+      }
     }
   }
 `;
