e7aca6e4d35309443212f5e39d63c619a599034c
Patrick Obermeier <pato@xxxlgroup.com>
2022-01-23 15:08:09 +0100

    update getBlogPost-query to fetch additional data

--- src/graphql/queries/getBlogPost.js
+++ src/graphql/queries/getBlogPost.js
@@ -2,7 +2,24 @@ const { gql } = require("graphql-request");
 
 const GET_BLOG_POST = gql`
   query GetBlogPost($slug: String!) {
+    site: _site {
+      logo: favicon {
+        url
+        tags
+        customData
+      }
+      favicon: faviconMetaTags {
+        attributes
+        content
+        tag
+      }
+    }
     blogPost(filter: { slug: { eq: $slug } }) {
+      seo: _seoMetaTags {
+        attributes
+        content
+        tag
+      }
       author {
         name
         photo {
