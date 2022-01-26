93064c6fee2f59354f901fa7d01dd038b9fd8ab5
Patrick Obermeier <pato@xxxlgroup.com>
2022-01-26 09:23:33 +0100

    FIX Email Obfuscation

--- src/hooks/useReplaceHrefInLink.ts
+++ src/hooks/useReplaceHrefInLink.ts
@@ -1,14 +1,27 @@
 import { useEffect } from "react";
+import Router from "next/router";
 
 const useReplaceHrefInAllMatchedLinks = (urlToMatch: string, replaceUrl: string) => {
   useEffect(() => {
-    const allLinks = document.querySelectorAll("a");
+    const updateLinks = () => {
+      setTimeout(() => {
+        const allLinks = document.querySelectorAll("a");
 
-    allLinks.forEach((link) => {
-      if (link.href === urlToMatch) {
-        link.href = replaceUrl;
-      }
-    });
+        allLinks.forEach((link) => {
+          if (link.href === urlToMatch) {
+            link.href = replaceUrl;
+          }
+        });
+      }, 1000);
+    };
+
+    updateLinks();
+
+    Router.events.on("routeChangeComplete", updateLinks);
+
+    return () => {
+      Router.events.off("routeChangeComplete", updateLinks);
+    };
   }, [replaceUrl, urlToMatch]);
 };
 
