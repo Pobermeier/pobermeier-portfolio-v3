9783b782ae88c33db760ad4e7a5a87f4b9ec66c6
Patrick Obermeier <pato@xxxlgroup.com>
2022-01-31 20:55:12 +0100

    create story for DarkModeToggle

---
+++ src/components/Buttons/DarkModeToggle.stories.tsx
@@ -0,0 +1,12 @@
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+// components
+import DarkModeToggle from "components/Buttons/DarkModeToggle";
+
+export default {
+  title: "DarkModeToggle",
+  component: DarkModeToggle,
+} as ComponentMeta<typeof DarkModeToggle>;
+
+const Template: ComponentStory<typeof DarkModeToggle> = () => <DarkModeToggle />;
+
+export const Default = Template.bind({});
